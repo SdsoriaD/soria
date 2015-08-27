30004a44d7276b7d09b4a03c6a9ddb65f651840c
Daniel <sdsoriasystems@gmail.com>
2015-08-27 15:17:08 -0500

    purueba de casa1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,8 @@
             <div class="content">
                 <div class="title">Laravel 5</div>
                 <div class="quote">{{ Inspiring::quote() }}</div>
-                <div class="quote">Hola</div>
+                <div class="quote">Hola miko</div>
+                <div class="quote">Hola chee!!!</div>
             </div>
         </div>
     </body>
